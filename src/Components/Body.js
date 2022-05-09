db65ea44422ef1cce1a5e73dc1d03b48d9bb5fae
UserPasha <pasharoma13@gmail.com>
2022-05-09 20:35:35 +0300

    start a new project

---
+++ src/Components/Body.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <div>
+          Hi Hi
+        </div>
+    );
+};
+
+export default Body;
---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            Hi Hi Hi
+        </div>
+    );
+};
+
+export default Footer;
